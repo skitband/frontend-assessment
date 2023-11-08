f620780142e4153b09de1c475f8b068ad39e06e8
ghost.lio <skitband00@gmail.com>
2023-11-08 23:30:51 +0800

    make it better

--- exercise_one/src/components/CardComponent.vue
+++ exercise_one/src/components/CardComponent.vue
@@ -1,18 +1,16 @@
-<script setup>
-  import { defineProps } from 'vue';
-
-  const props = defineProps({
-    cardData: {
-      type: Array,
+<script>
+  export default {
+    props: {
+        cardData: Array,
     },
-  });
+  };
 </script>
 
 <template>
   <div class="album py-5">
     <div class="container">
       <div class="row justify-content-center g-4">
-        <div v-for="(card, index) in props.cardData" :key="index" class="col-12 col-md-4 custom-card">
+        <div v-for="(card, index) in cardData" :key="index" class="col-12 col-md-4 custom-card">
           <div class="custom-card__wrapper card shadow-sm">
             <img class="custom-card__image img p-3" :src="card.imgSrc" alt="card thumbnail" />
             <div class="custom-card__body card-body d-flex flex-column">
